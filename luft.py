cc72a3b7d53e10624f2c2677c2a3561a69c0ac28
Mitja Schmakeit <mitja@mitja.cc>
2020-12-05 15:31:53 +0000

    update to newer library, old one is discontinued

--- luft.py
+++ luft.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-import Adafruit_DHT
 import time
+import board
+import adafruit_dht
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
+dht_device = adafruit_dht.DHT22(board.D4)
 
 while True:
     time.sleep(2)
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-    else:
+    try:
+        temperature_c = dht_device.temperature
+        humidity = dht_device.humidity
+        print("Temp={0:0.1f}ºC  Humidity={1:0.1f}%".format(temperature_c, humidity))
+    except RuntimeError as error:
+        print(error.args[0])
+    except Exception as error:
+        dht_device.exit()
         print("Failed to retrieve data from humidity sensor")
+        raise error
 
