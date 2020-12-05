61f581fe1a6087f109c20e0956175eb319684334
Mitja Schmakeit <mitja@mitja.cc>
2020-12-05 18:37:20 +0000

    publish_own

--- luft.py
+++ luft.py
@@ -2,30 +2,75 @@
 import time
 import board
 import adafruit_dht
-import homie
+import paho.mqtt.client as mqtt
 
 dht_device = adafruit_dht.DHT22(board.D4)
 
-Homie = homie.Homie(homie.loadConfigFile("homie.json"))
-h_temperature_node = Homie.Node("temperature", "temperature")
-h_humidity_node = Homie.Node("humidity", "humidity")
-Homie.setFirmware("awesome-temperature", "1.0.0")
-h_temperature_node.advertise("degrees")
-h_humidity_node.advertise("humidity")
-Homie.setup()
+mqttc = mqtt.Client()
+mqttc.connect("10.10.10.8")
+
+h_id = "air01"
+h_name = "Air 01"
+
+tb = f"homie/{h_id}"
+tbd = f"{tb}/dht22"
+
+def publish_retained(topic, payload):
+    mqttc.publish(f"{tb}/{topic}", payload=payload)
+
+publish_retained("$state", "init")
+
+publish_retained("$homie", "3.0.1")
+
+mqttc.loop()
+
+publish_retained("$name", h_name)
+publish_retained("$nodes", "dht22")
+publish_retained("$stats", "uptime")
+mqttc.loop()
+
+publish_retained("dht22/$name", "DHT22 Sensor")
+publish_retained("dht22/$type", "Senseless Homie Type")
+publish_retained("dht22/$properties", "temperature,humidity")
+mqttc.loop()
+
+publish_retained("dht22/temperature/$name", "Lufttemperatur")
+publish_retained("dht22/temperature/$datatype", "float")
+publish_retained("dht22/temperature/$unit", "ºC")
+mqttc.loop()
+
+publish_retained("dht22/humidity/$name", "Luftfeuchtigkeit")
+publish_retained("dht22/humidity/$datatype", "float")
+publish_retained("dht22/humidity/$unit", "%")
+mqttc.loop()
+
+publish_retained("$state", "ready")
+mqttc.loop()
+
+uptime = 0
+sleep_seconds = 5
+
+def send_stats():
+    publish_retained("$stats/uptime", f"{uptime}")
 
 while True:
-    time.sleep(5)
+    mqttc.loop()
+
+    if uptime % 30 == 0:
+        send_stats()
+
+    uptime += sleep_seconds
+    time.sleep(sleep_seconds)
+
     try:
         temperature_c = dht_device.temperature
         humidity = dht_device.humidity
         print("Temp={0:0.1f}ºC  Humidity={1:0.1f}%".format(temperature_c, humidity))
-        h_temperature_node.setProperty("degrees").send(temperature_c)
-        h_humidity_node.setProperty("humidity").send(humidity)
+        publish_retained("dht22/temperature", f"{temperature_c}")
+        publish_retained("dht22/humidity", f"{humidity}")
     except RuntimeError as error:
         print(error.args[0])
     except Exception as error:
         dht_device.exit()
-        print("Failed to retrieve data from humidity sensor")
         raise error
 
