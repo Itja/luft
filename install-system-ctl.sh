01094bdc30286de7d8ef48df04f94a058539f93d
Mitja Schmakeit <mitja@mitja.cc>
2020-12-15 20:56:38 +0000

    Luft As A Service (LAAS)'

---
+++ install-system-ctl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+systemctl stop luft
+cp luft.service /etc/systemd/system/
+systemctl enable luft
+if (systemctl start luft); then
+  sleep 2
+  if (systemctl is-active luft >/dev/null); then
+    echo 'Kadse service has been started successfully!'
+  else
+    echo 'ERROR: Failure after starting.'
+  fi
+  echo ''
+  journalctl -u luft -n 5 -o short --no-pager
+else
+  echo 'ERROR: The updater service could not be started.'
+fi
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Waiting for 10 seconds.."
+sleep 10
+/usr/bin/python3 luft.py
