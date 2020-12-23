4016de1df9a76c6109d250f2c0ef592a9ba87f2c
Mitja Schmakeit <mitja@mitja.cc>
2020-12-23 23:51:17 +0000

    fixed Homie4 reconnection bug

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 git pull
+sudo python3 -m pip uninstall Homie4
+git clone --branch fix/mqtt-disconnect-property https://github.com/Itja/Homie4.git ../Homie4
+ln -s ../Homie4/homie ./homie
 sudo ./install-system-ctl.sh
-
